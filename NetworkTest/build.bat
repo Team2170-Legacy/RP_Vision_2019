67263f10ddaef16e377fbe4ce8fea4caaeadf815
KnockKnockSeven <memorybytes24@gmail.com>
2019-02-02 14:21:37 -0500

    Initial Network Table Functionality
    Test program to write a value to a network table.

--- NetworkTest/build.bat
+++ NetworkTest/build.bat
@@ -2,6 +2,9 @@ set /P ip=Enter PI IP:
 set /P copyfilename=Enter Executable Filename:
 make
 pause
-scp %copyfilename% pi@%ip%:~/RP_Vision_2019/NetworkTables/%copyfilename%
+scp %copyfilename% pi@%ip%:~/RP_Vision_2019/NetworkTest/%copyfilename%
 pause
-scp GripPipeline.o pi@%ip%:~/RP_Vision_2019/NetworkTables/GripPipeline.o
+scp %copyfilename% pi@%ip%:~/RP_Vision_2019/NetworkTest/%copyfilename%.o
+pause
+scp GripPipeline.o pi@%ip%:~/RP_Vision_2019/NetworkTest/GripPipeline.o
+pause
