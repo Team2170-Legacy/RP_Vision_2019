df5287082294f8528b5caa8c12939f505c904058
Priyanshu4 <35632577+Priyanshu4@users.noreply.github.com>
2019-02-09 09:33:54 -0500

    Edited VisionTarget_v2
    Added code to draw rectangles over contours

--- VisionTarget/VisionTarget_v2(inProgress).cpp
+++ VisionTarget/VisionTarget_v2(inProgress).cpp
@@ -99,12 +99,53 @@ int main(){
 	}
 	}
 	return 0;
-
-
 };
 
-// method to convert contours to mat(image)
+cv::Mat detect_rectangles(std::vector<std::vector<cv::Point>> contours)
+{
+    // compute mask (you could use a simple threshold if the image is always as good as the one you provided)
+    cv::Mat mask;
+    std::vector<cv::Vec4i> hierarchy;
+    cv::findContours(mask,contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
 
-cv::Mat* contoursToMat(std::vector<std::vector<cv::Point> > contours) {
-return cv::Mat;
+    /// Draw contours and find biggest contour (if there are other contours in the image, we assume the biggest one is the desired rect)
+    // drawing here is only for demonstration!
+    int biggestContourIdx = -1;
+    float biggestContourArea = 0;
+    cv::Mat drawing = cv::Mat::zeros( mask.size(), CV_8UC3 );
+    for( int i = 0; i< contours.size(); i++ )
+    {
+        cv::Scalar color = cv::Scalar(0, 100, 0);
+        drawContours( drawing, contours, i, color, 1, 8, hierarchy, 0, cv::Point() );
+
+        float ctArea= cv::contourArea(contours[i]);
+        if(ctArea > biggestContourArea)
+        {
+            biggestContourArea = ctArea;
+            biggestContourIdx = i;
+        }
+    }
+
+    // if no contour found
+    if(biggestContourIdx < 0)
+    {
+        std::cout << "no contour found" << std::endl;
+        return 1;
+    }
+
+    // compute the rotated bounding rect of the biggest contour! (this is the part that does what you want/need)
+    cv::RotatedRect boundingBox = cv::minAreaRect(contours[biggestContourIdx]);
+    // one thing to remark: this will compute the OUTER boundary box, so maybe you have to erode/dilate if you want something between the ragged lines
+
+
+
+    // draw the rotated rect
+    cv::Point2f corners[4];
+    boundingBox.points(corners);
+    cv::line(drawing, corners[0], corners[1], cv::Scalar(255,255,255));
+    cv::line(drawing, corners[1], corners[2], cv::Scalar(255,255,255));
+    cv::line(drawing, corners[2], corners[3], cv::Scalar(255,255,255));
+    cv::line(drawing, corners[3], corners[0], cv::Scalar(255,255,255));
+
+	return drawing;
 }
