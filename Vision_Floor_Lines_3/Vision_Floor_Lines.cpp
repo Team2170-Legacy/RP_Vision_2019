7cece48e45fd2b9eeaa311f5d8ae9bbdf0a3f54a
Nikhi Boggavarapu <nikhibgg@gmail.com>
2019-02-19 19:44:02 -0500

    Indentation of code after if statement
    Someones gotta read these ~ Najeed

--- Vision_Floor_Lines_3/Vision_Floor_Lines.cpp
+++ Vision_Floor_Lines_3/Vision_Floor_Lines.cpp
@@ -16,16 +16,16 @@
 int main()
 
 {
-//    static void VisionThread()
+        //    static void VisionThread()
 
-//    {
+        //    {
 
         grip::GripPipeline pipeline;
 
         cs::UsbCamera camera = frc::CameraServer::GetInstance()->StartAutomaticCapture();
 
-        int     width = 160;
-        int     height = 120;
+        int width = 160;
+        int height = 120;
         camera.SetResolution(width, height);
 
         cs::CvSink cvSink = frc::CameraServer::GetInstance()->GetVideo();
@@ -36,11 +36,11 @@ int main()
 
         cv::Mat output;
 
-        cv::Mat* output_ptr;
+        cv::Mat *output_ptr;
 
-        std::vector<std::vector<cv::Point> >* contours_ptr;
+        std::vector<std::vector<cv::Point>> *contours_ptr;
 
-        std::vector<std::vector<cv::Point> > contours;
+        std::vector<std::vector<cv::Point>> contours;
 
         std::vector<cv::Point> contour1;
 
@@ -51,139 +51,141 @@ int main()
         cv::Point tl1;
         cv::Point br2;
         cv::Point br1;
-// read in image file
-//    Mat image; 
+        // read in image file
+        //    Mat image;
 
-//******    source = cv::imread("Test_Image", CV_LOAD_IMAGE_COLOR);
-    
-    	uint64_t grab_Frame_Status = -1;
+        //******    source = cv::imread("Test_Image", CV_LOAD_IMAGE_COLOR);
 
-//     if(! source.data )
-//     {
-//             std::cout <<  "Could not open or find the image" << std::endl ;
-//             return -1;
-//     }
-  
-        while(true) 
+        uint64_t grab_Frame_Status = -1;
+
+        //     if(! source.data )
+        //     {
+        //             std::cout <<  "Could not open or find the image" << std::endl ;
+        //             return -1;
+        //     }
+
+        while (true)
         {
-	grab_Frame_Status = cvSink.GrabFrame(source);
-//        std::cout << "Grab Frame Status: " << grab_Frame_Status << std::endl;
+                grab_Frame_Status = cvSink.GrabFrame(source);
+                std::cout << "Grab Frame Status: " << grab_Frame_Status << std::endl;
 
-
-
-
-        output = source;
-            if ( source.rows > 0)
-            {
-//**               cvtColor(source, output, cv::COLOR_BGR2GRAY);
-                pipeline.Process(source);
-                contours_ptr = pipeline.GetFilterContoursOutput();
-                contours = *contours_ptr;
-
-                contour1 =contours[0];
-
-                r1 = cv::boundingRect(contour1);
-//                r2 = cv::boundingRect(contour2);
-
-                tl1 = r1.tl();
-                tl2 = r2.tl();
-                br1 = r1.br();
-                br2 = r2.br();
-                std::cout << "Top left of rectangle x: (" << tl1.x << ")" << std::endl;
-                std::cout << "Top left of rectangle y: (" << tl1.y << ")" << std::endl;
-                std::cout << "Bottom right of rectangle x: (" << br1.x << ")" << std::endl;
-                std::cout << "Bottom right of rectangle y: (" << br1.y << ")" << std::endl;
-                std::vector<cv::Rect> boundingBoxArray;
-
-                //boundingBoxArray.push_back(r1);
-
- //               boundingBoxArray[1] = r1;
-
-                int num_contours = contours.size();
-                int midpointBox [num_contours];
-                int minimum = 0;
- //               boundingBoxArray[0] = cv::boundingRect(contours[0]);
-
-                for(int count = 0; count < num_contours; count++){        
-                       boundingBoxArray.push_back(cv::boundingRect(contours[count]));
-                        int midx = ( (boundingBoxArray[count].tl()).x + (boundingBoxArray[count].br()).x ) / 2;
-                        int midy = ( (boundingBoxArray[count].tl()).y + (boundingBoxArray[count].br()).y ) / 2;
-                       midpointBox[count] = midx;
-                }
-
-                int differenceMidpoint [num_contours];
-                for(int count = 0; count < num_contours; count++){
-                        differenceMidpoint[count] = abs((width/2) - midpointBox[count]);
-                }
-                for(int count = 1; count < num_contours; count++){
-                        if(differenceMidpoint[count] < differenceMidpoint[minimum]){
-                                minimum = count;
-                        }
-                }
-
-        {
-                std::cout <<"Minimum: (" << minimum << ")" << std::endl;
-                std::cout <<"Midpoint x: (" << midpointBox[minimum] << ")" <<std::endl; 
-        }
-             //output_ptr = pipeline.GetCvApplycolormapOutput();
-             //output = *output_ptr;
-                output = source;
-                cv::Scalar color = cv::Scalar(0,0,255); //BGR Red Red
-
-//                tl1.x = 20;
-//                tl1.y = 20;
-//                br1.x = 60;
-//                br1.y = 60;
-
-                int THICKNESS = 1;
-                cv::rectangle(output, boundingBoxArray[minimum].tl(),boundingBoxArray[minimum].br(), color, THICKNESS+3, 8, 0);
-//                cv::rectangle(output, tl2, br2, color, 4, 8, 0);
-
-
-                int midxr = ( (boundingBoxArray[minimum].tl()).x + (boundingBoxArray[minimum].br()).x ) / 2;
-                //Draw a THICC circle
-                cv::Point midpoint(midxr, ((boundingBoxArray[minimum].tl()).y));
-                cv::circle(output, midpoint, 5, color, THICKNESS, 8, 0);
-
-                cv::Scalar color2 = cv::Scalar(255,255,255);
-                if(num_contours > 1){
-                        for(int otherBox = 0; otherBox < num_contours; otherBox++){
-                                if(otherBox != minimum){
-                                        cv::rectangle(output, boundingBoxArray[otherBox].tl(), boundingBoxArray[otherBox].br(), color2, THICKNESS, 8, 0);
-                                }
-                        }
-                }
-
-                // for(int count = 0; count < num_contours; count++){
-                //         cv::circle(output, midpoint, 1, color)
-                // }
-
-
-//MK        outputStreamStd.PutFrame(source);
-
-//          std::this_thread::sleep_for (std::chrono::milliseconds(100));
-            }  // if ( source.rows > 0)
- //       }
-
-        //if ( output.rows > 0)
-        if(! source.data )
-
+                if (grab_Frame_Status > -1)
                 {
-                        //std::cout << "output.rows = 0...." << std::endl;
-                        std::cout << "! source.data is TRUE...." << std::endl;
-                }
-        else                                
-                outputStreamStd.PutFrame(output);
 
-                
+                        output = source;
+                        if (source.rows > 0)
+                        {
+                                //**               cvtColor(source, output, cv::COLOR_BGR2GRAY);
+                                pipeline.Process(source);
+                                contours_ptr = pipeline.GetFilterContoursOutput();
+                                contours = *contours_ptr;
 
-    } // while(true) 
+                                contour1 = contours[0];
 
-//    std::thread visionThread(VisionThread);
+                                r1 = cv::boundingRect(contour1);
+                                //                r2 = cv::boundingRect(contour2);
 
-//    visionThread.detach();
+                                tl1 = r1.tl();
+                                tl2 = r2.tl();
+                                br1 = r1.br();
+                                br2 = r2.br();
+                                std::cout << "Top left of rectangle x: (" << tl1.x << ")" << std::endl;
+                                std::cout << "Top left of rectangle y: (" << tl1.y << ")" << std::endl;
+                                std::cout << "Bottom right of rectangle x: (" << br1.x << ")" << std::endl;
+                                std::cout << "Bottom right of rectangle y: (" << br1.y << ")" << std::endl;
+                                std::vector<cv::Rect> boundingBoxArray;
+
+                                //boundingBoxArray.push_back(r1);
+
+                                //               boundingBoxArray[1] = r1;
+
+                                int num_contours = contours.size();
+                                int midpointBox[num_contours];
+                                int minimum = 0;
+                                //               boundingBoxArray[0] = cv::boundingRect(contours[0]);
+
+                                for (int count = 0; count < num_contours; count++)
+                                {
+                                        boundingBoxArray.push_back(cv::boundingRect(contours[count]));
+                                        int midx = ((boundingBoxArray[count].tl()).x + (boundingBoxArray[count].br()).x) / 2;
+                                        int midy = ((boundingBoxArray[count].tl()).y + (boundingBoxArray[count].br()).y) / 2;
+                                        midpointBox[count] = midx;
+                                }
+
+                                int differenceMidpoint[num_contours];
+                                for (int count = 0; count < num_contours; count++)
+                                {
+                                        differenceMidpoint[count] = abs((width / 2) - midpointBox[count]);
+                                }
+                                for (int count = 1; count < num_contours; count++)
+                                {
+                                        if (differenceMidpoint[count] < differenceMidpoint[minimum])
+                                        {
+                                                minimum = count;
+                                        }
+                                }
+
+                                {
+                                        std::cout << "Minimum: (" << minimum << ")" << std::endl;
+                                        std::cout << "Midpoint x: (" << midpointBox[minimum] << ")" << std::endl;
+                                }
+                                //output_ptr = pipeline.GetCvApplycolormapOutput();
+                                //output = *output_ptr;
+                                output = source;
+                                cv::Scalar color = cv::Scalar(0, 0, 255); //BGR Red Red
+
+                                //                tl1.x = 20;
+                                //                tl1.y = 20;
+                                //                br1.x = 60;
+                                //                br1.y = 60;
+
+                                int THICKNESS = 1;
+                                cv::rectangle(output, boundingBoxArray[minimum].tl(), boundingBoxArray[minimum].br(), color, THICKNESS + 3, 8, 0);
+                                //                cv::rectangle(output, tl2, br2, color, 4, 8, 0);
+
+                                int midxr = ((boundingBoxArray[minimum].tl()).x + (boundingBoxArray[minimum].br()).x) / 2;
+                                //Draw a THICC circle
+                                cv::Point midpoint(midxr, ((boundingBoxArray[minimum].tl()).y));
+                                cv::circle(output, midpoint, 5, color, THICKNESS, 8, 0);
+
+                                cv::Scalar color2 = cv::Scalar(255, 255, 255);
+                                if (num_contours > 1)
+                                {
+                                        for (int otherBox = 0; otherBox < num_contours; otherBox++)
+                                        {
+                                                if (otherBox != minimum)
+                                                {
+                                                        cv::rectangle(output, boundingBoxArray[otherBox].tl(), boundingBoxArray[otherBox].br(), color2, THICKNESS, 8, 0);
+                                                }
+                                        }
+                                }
+
+                                // for(int count = 0; count < num_contours; count++){
+                                //         cv::circle(output, midpoint, 1, color)
+                                // }
+
+                                //MK        outputStreamStd.PutFrame(source);
+
+                                //          std::this_thread::sleep_for (std::chrono::milliseconds(100));
+                        } // if ( source.rows > 0)
+                        //       }
+
+                        //if ( output.rows > 0)
+                        if (!source.data)
+
+                        {
+                                //std::cout << "output.rows = 0...." << std::endl;
+                                std::cout << "! source.data is TRUE...." << std::endl;
+                        }
+                        else
+                                outputStreamStd.PutFrame(output);
+
+                } // if(grab_Frame_Status > -1)
+        }         // while(true)
+
+        //    std::thread visionThread(VisionThread);
+
+        //    visionThread.detach();
 
         return 0;
-
-
 };
