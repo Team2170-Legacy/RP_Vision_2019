950bcfa221151979d8a8021f6f4a35edccb80f07
Martin Krucinski <mkrucinski@gmail.com>
2019-03-05 17:02:41 -0500

    Added exposure setting for VisionTarget camera via Network Tables

--- VisionTarget/VisionTarget.cpp
+++ VisionTarget/VisionTarget.cpp
@@ -19,14 +19,19 @@
 #include "networktables/NetworkTable.h"
 #include "networktables/NetworkTableEntry.h"
 #include "networktables/NetworkTableInstance.h"
+#include "networktables/EntryListenerFlags.h"
+
+//----------------------------------------------------------------------------------------------
+
+bool		debug = false; // set to true for debugging: additional video streams etc.
 
 // data about locked contours, updated whenever a target is locked on too
-double targetWidth = 0; // width of bounding box of locked vision targets
-double targetHeight = 0; // height of bounding box of locked vision targets
-int targetCenterX = 0; // x value of center coordinate of bounding box around locked vision targets
-bool targetLocked = false; // whether or not a target is currently locked
-double leftTapeHeight = 0; // height of left tape in locked target
-double rightTapeHeight = 0; // height of right tape in locked target
+double 	targetWidth = 0; // width of bounding box of locked vision targets
+double 	targetHeight = 0; // height of bounding box of locked vision targets
+int 		targetCenterX = 0; // x value of center coordinate of bounding box around locked vision targets
+bool 		targetLocked = false; // whether or not a target is currently locked
+double 	leftTapeHeight = 0; // height of left tape in locked target
+double 	rightTapeHeight = 0; // height of right tape in locked target
 
 
 /*
@@ -39,6 +44,8 @@ void write_log(string message)
 		myfile.close();
 }
 */
+
+//----------------------------------------------------------------------------------------------
 
 cv::Mat detect_rectangles(cv::Mat source, std::vector<std::vector<cv::Point>> contours, cv::Scalar color, int thickness, bool updateLockedTargetData)
 {
@@ -54,9 +61,8 @@ cv::Mat detect_rectangles(cv::Mat source, std::vector<std::vector<cv::Point>> co
 	cv::Point2f* bottomRight;
 	std::vector<std::vector<float>> bounding;
 
-
-double contourHeights [2] = {0 , 0};
-float contourMinXs [2] = {0 , 0};
+	double contourHeights [2] = {0 , 0};
+	float contourMinXs [2] = {0 , 0};
 	for (int c = 0; c < 2; c++)
 	{
 		cv::RotatedRect boundingBox = cv::minAreaRect(contours[c]);
@@ -108,11 +114,13 @@ float contourMinXs [2] = {0 , 0};
 	rightTapeHeight = contourHeights[0];
 	}
 
-	}
+}
 
 
 	return drawing;
-}
+} //cv::Mat detect_rectangles
+
+//----------------------------------------------------------------------------------------------
 
 cv::Mat lock_target(cv::Mat source, std::vector<std::vector<cv::Point>> contours, int width)
 {
@@ -183,8 +191,9 @@ cv::Mat lock_target(cv::Mat source, std::vector<std::vector<cv::Point>> contours
 	}
 
 	return source;
-}
+} //cv::Mat lock_target
 
+//----------------------------------------------------------------------------------------------
 // given height of the bounding box of a locked target calulates the distance in feet the robot is away from the target
 double calcDistance(double height){
 		double yCoordArr [11] = {20, 22, 24, 27, 30, 36, 43, 55, 62, 70, 134};
@@ -211,22 +220,32 @@ double calcDistance(double height){
 	return distance;
 
 
-}
+} // double calcDistance
+
+//----------------------------------------------------------------------------------------------
 
 int main() {
 	grip::GripPipeline pipeline;
 	
 	// camera setup
 	int cWidth = 320;
-    int cHeight = 240;
+  int cHeight = 240;
+
 	int cExposure = 2;
 	int cWhiteBalance = 5100;
+
+  int exposure_test_counter = 0;
+	int cExposure_temp  = 2;
+
 	cs::UsbCamera camera = frc::CameraServer::GetInstance()->StartAutomaticCapture();
 	camera.SetResolution(cWidth, cHeight);
 	camera.SetExposureManual(cExposure);
 	camera.SetWhiteBalanceManual(cWhiteBalance);
 	cs::CvSink cvSink = frc::CameraServer::GetInstance()->GetVideo();
+	// MK 03/05/2019 Can the line below be commented out so that we for sure don't send any additional
+	// video streams when debug = false?
 	cs::CvSource outputStreamStd = frc::CameraServer::GetInstance()->PutVideo("hsvoutput", cWidth, cHeight);
+
 	cs::CvSource outputStreamRectStd = frc::CameraServer::GetInstance()->PutVideo("countouroutput", cWidth, cHeight);
 	cv::Mat source;
 	cv::Mat output;
@@ -236,6 +255,8 @@ int main() {
 	std::vector<std::vector<cv::Point> >* contours_ptr;
 	std::vector<std::vector<cv::Point> > contours;
 
+	//----------------------------------------------------------------------------------------------
+
 	//network tables setup
 	unsigned int port = 1735;
 	auto inst = nt::NetworkTableInstance::GetDefault();
@@ -244,14 +265,31 @@ int main() {
 	inst.StartClient("10.21.70.2",port);
 	auto table = inst.GetTable("VisionTable");
 	nt::NetworkTableEntry x_target_error =  table->GetEntry("x_target_error");
+
+	//----------------------------------------------------------------------------------------------
+	// camera exposure esttings
+	//----------------------------------------------------------------------------------------------
+
+	nt::NetworkTableEntry exposure =  table->GetEntry("exposure");
+	exposure.SetDouble(2);
+	//nt::NetworkTableEntry exposure2 =  table->GetEntry("exposure2");
+
+	// the example below had JAVA syntax, wait converting this, use the getvalue differently
+	//exposure.addListener(event -> {cexposure_temp = event.value.getValue();},
+	//	EntryListenerFlags.kNew | EntryListenerFlags.kUpdate);
+
+	//----------------------------------------------------------------------------------------------
 	while (true) {
 		cvSink.GrabFrame(source);
 		if (source.rows > 0) {
 		// outputStreamRectStd.PutFrame(source);
 			pipeline.Process(source);
-			hsv_mat_ptr = pipeline.GetHsvThresholdOutput();
-			hsv_mat = *hsv_mat_ptr;
-			outputStreamStd.PutFrame(hsv_mat);
+			if ( debug )
+			{
+				hsv_mat_ptr = pipeline.GetHsvThresholdOutput();
+				hsv_mat = *hsv_mat_ptr;
+				outputStreamStd.PutFrame(hsv_mat);
+			};
 			contours_ptr = pipeline.GetFilterContoursOutput();
 			contours = *contours_ptr;
 			if (!contours.empty()) {
@@ -268,19 +306,17 @@ int main() {
 				//right_tape_height.SetDouble(0);
 			}
   
- nt::NetworkTableEntry target_locked =  table->GetEntry("target_locked");
- target_locked.SetBoolean(targetLocked);
+ 			nt::NetworkTableEntry target_locked =  table->GetEntry("target_locked");
+ 			target_locked.SetBoolean(targetLocked);
 			if(targetLocked) {
-    
-	x_target_error.SetDouble(targetCenterX - (cWidth/2));
-	  nt::NetworkTableEntry distance_to_target =  table->GetEntry("distance_to_target");
-	distance_to_target.SetDouble(calcDistance(targetHeight));
-	nt::NetworkTableEntry left_tape_height =  table->GetEntry("left_tape_height");
-	nt::NetworkTableEntry right_tape_height =  table->GetEntry("right_tape_height");
-	left_tape_height.SetDouble(leftTapeHeight);
-	right_tape_height.SetDouble(rightTapeHeight);
-	//std::cout << calcDistance(targetWidth) << std::endl;
-	
+				x_target_error.SetDouble(targetCenterX - (cWidth/2));
+	  		nt::NetworkTableEntry distance_to_target =  table->GetEntry("distance_to_target");
+				distance_to_target.SetDouble(calcDistance(targetHeight));
+				nt::NetworkTableEntry left_tape_height =  table->GetEntry("left_tape_height");
+				nt::NetworkTableEntry right_tape_height =  table->GetEntry("right_tape_height");
+				left_tape_height.SetDouble(leftTapeHeight);
+				right_tape_height.SetDouble(rightTapeHeight);
+				//std::cout << calcDistance(targetWidth) << std::endl;
 			}
 			else {
 				x_target_error.SetDouble(0);
@@ -288,9 +324,29 @@ int main() {
 				//right_tape_height.SetDouble(0);
 			}
 
+			// Expsoure testing
+ 			
+
+			//if (exposure_test_counter > 50)
+			//{
+			//	exposure_test_counter = 0;
+
+				// testing before network tables:
+				//camera.SetExposureManual(cExposure_temp);
+				//cExposure_temp = 8 - cExposure_temp;
+
+				// Use code below for reading the exposure values from network table
+			//}
+			//else
+			//	exposure_test_counter = exposure_test_counter + 1;
+				
+			cExposure_temp 	= table->GetNumber("exposure",2);
+			camera.SetExposureManual(cExposure_temp);
 			
-		}
-	}
+
+			
+		} //if (source.rows > 0) 
+	} // while (true)
 	
 	return 0;
 
